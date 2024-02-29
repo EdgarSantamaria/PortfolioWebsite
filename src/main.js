18747f8f36525549bacb02b7b7b437e83731938b
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-02-28 19:54:02 -0500

    revamped entire website
    updated styles
    updated nav bar
    added about me section
    added projects section

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css';
+import '../styles/components/header.css'
+import '../styles/components/intro.css'
+import '../styles/components/about.css'
+import '../styles/components/work.css'
+import '../styles/utils.css';
