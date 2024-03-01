4dc477bd642d343557dbe4138dac4d29f0bf8484
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-03-01 09:48:42 -0500

    added js features
    dark mode, lazy loading, mobile menu, and style updated

--- src/main.js
+++ src/main.js
@@ -10,5 +10,9 @@ import '../styles/components/mobile-nav.css'
 import '../styles/utils.css';
 
 import mobileNav from './utils/mobile-nav.js';
+import darkMode from './utils/dark-mode.js';
+import lazyLoading from './utils/lazy-loading.js';
 
+lazyLoading();
+darkMode();
 mobileNav();
